cc2f94189b5f024219508b163c5c92c44bb5870e
Alfredo <juan.callisaya.57751@usalesiana.edu.bo>
2025-12-02 08:39:20 -0400

    feat: Agregar la peticion put con fecha y hora

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const fecha = new Date();
 
+dia = ("día:", fecha.getDay());
+mes = ("mes:", fecha.getMonth());
+año = ("año:", fecha.getFullYear());
+hora = ("hora:", fecha.getHours());
+minutos = ("hora:", fecha.getMinutes());
+segundos = ("hora:", fecha.getSeconds());
+mSegundos = ("hora:", fecha.getMilliseconds());
 // Middleware para parsear JSON
 app.use(express.json());
 
@@ -26,13 +34,23 @@ app.post("/datos", (req, res) => {
       correo: datos.correo,
       edad: datos.edad,
     },
-    fechaRegistro: new Date(),
+    
+
+    fechaRegistro: dia,
     horaRegistro: Date.now(),
   });
 });
 
 app.put("/datos/:id", (req, res) => {
-  
+  const id = req.params.id;
+  const datos = req.body;
+  res.json({
+    mensaje: `Datos actualizados exitosamente`,
+    idUsuario: id,
+    datosActualizados: datos,
+    fechaActualizacion: `${año}-${mes}-${dia} ${hora}:${minutos}:${segundos}.${mSegundos}`,
+    horaActualizacion: Date.now(),
+  });
 });
 
 app.listen(PORT, () => {
