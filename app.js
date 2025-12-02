9327b30294206dc5278241dda126b2893eb6ecbd
Alfredo <juan.callisaya.57751@usalesiana.edu.bo>
2025-12-02 08:08:18 -0400

    feat: Agregar la peticion get

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get("/estado", (req, res) => {
-  
+  res.send({
+    servicio: "Proyecto-backend",
+    version: "1.11.2",
+    entorno: "production",
+    estado: "Servicio funcionando correctamente",
+    fecha: new Date(),
+    hora: Date.now(),
+  });
 });
 
 app.post("/datos", (req, res) => {
